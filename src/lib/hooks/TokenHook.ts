cab8773433da4ae9cac4a5a279a50cbf0f121cda
Moataz <moataz_abdullah@outlook.com>
2025-09-04 08:17:37 +0300

    final commit for deployment fixes

--- src/lib/hooks/TokenHook.ts
+++ src/lib/hooks/TokenHook.ts
@@ -4,8 +4,10 @@ import { useEffect, useState } from "react";
 export default function useToken() {
   const [Token, setToken] = useState<string | null>(null);
   useEffect(() => {
-    const tempToken = localStorage.getItem("token");
-    setToken(tempToken);
+    if (typeof window !== "undefined") {
+      const tempToken = localStorage.getItem("token");
+      setToken(tempToken);
+    }
   }, []);
   return Token;
 }
